67d51effba9775866c0f6faeaf5497891f4500e1
Sadegh <mr.kamalou@gmail.com>
2021-07-08 10:43:30 +0430

    add

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace GitProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Sadegh, How Are You?");
+            Console.WriteLine("Hello Sadegh, How Are You?");
         }
     }
 }
