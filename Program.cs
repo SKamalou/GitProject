f1fd75e9625d11581085e20ff668097fe119ee03
Sadegh Kamalou <mr.kamalou@gmail.com>
2021-07-10 09:43:07 +0430

    First vs commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GitProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Sadegh, How Are You? Are you Okey?");
+            Console.WriteLine("Hello Sadegh, How Are You? Are you Okey? (Visual Studio)");
         }
     }
 }
